28c1d53be81cae039938decbc2de1118c7f4dc84
Aidan <grieve@pdx.edu>
2023-03-08 18:33:18 -0800

    created necessary files, began setting up general shape of my page. Started styling and experimenting with layout

---
+++ site.js
@@ -0,0 +1,10 @@
+const aboutPage = () => {};
+
+const workPage = () => {};
+
+const projectPage = () => {};
+
+const contactPage = () => {};
+
+// add onclick listeners to each tab in navbar
+// automatically start on about page
